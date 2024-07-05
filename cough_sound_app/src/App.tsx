c40df62287552b4866395eee856a99bd89978b0b
TorchFah <panupongtongfah@gmail.com>
2024-07-05 16:38:04 +0900

    feat(app): init application

---
+++ cough_sound_app/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import WelcomePage from './pages/WelcomePage';
+
+const App: React.FC = () => {
+    return (
+        <div className="App">
+            <WelcomePage />
+        </div>
+    );
+}
+
+export default App;
